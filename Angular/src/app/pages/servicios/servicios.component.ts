bfcc148f3f4734269150f84a4c7c1f08cf8f8243
Margarita-Bravo <bravo_m33@hotmail.com>
2023-05-17 21:53:42 -0300

    api prueba

--- Angular/src/app/pages/servicios/servicios.component.ts
+++ Angular/src/app/pages/servicios/servicios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicioService } from 'src/app/service/servicio.service';
 
 @Component({
   selector: 'app-servicios',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servicios.component.css']
 })
 export class ServiciosComponent implements OnInit {
+  
+  miServi:any;
+  //hoy = new Date();
+  //mostrarServicios:boolean=true;
+  //servicios:any;
 
-  constructor() { }
+  constructor(private serv: ServicioService) {
+ 
+    this.serv.obtenerServicios().subscribe({
+      next:(serviciosTodos)=>{
+        this.miServi=serviciosTodos;
+        console.log(" Exito se cargo los servicios");
+      },
+      error:(errorData)=> {
+        console.log("error del componenete servicio ");
+        console.error(errorData);
+      }
+    });
+  
+    
+   }
 
   ngOnInit(): void {
+  //   this.serv.obtenerServicios().subscribe(data=>{
+  //     // console.log(data);
+  //     this.miServi=data;
+  //     console.log(this.miServi);
+  //   });
   }
 
 }
