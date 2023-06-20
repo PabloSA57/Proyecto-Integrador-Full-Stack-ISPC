2ffb91ca14276f6726ac1cae10deff9cf6065ed6
mauriciojfranco <mauriciojfranco@live.com>
2023-06-20 16:17:28 -0300

    Corrección de home

--- Angular/src/app/service/producto.service.ts
+++ Angular/src/app/service/producto.service.ts
@@ -37,7 +37,7 @@ export class ProductoService {
 public create(data:any):Observable<any>{
   //return this.http.post(this.url,data);
   console.log('servicio ' + data.nombre)
-  return this.http.post(this.url +'producto/',data );
+  //return this.http.post(this.url +'producto/',data );
   return this.http.post(`${this.url}producto/`, data);
 
  } 
