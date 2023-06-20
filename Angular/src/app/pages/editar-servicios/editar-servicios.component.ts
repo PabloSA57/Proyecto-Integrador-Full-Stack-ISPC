c628ac47bcbbffe352b0cd688c178c881cc4386a
Margarita-Bravo <bravo_m33@hotmail.com>
2023-06-19 21:25:57 -0300

    crud servicio

--- Angular/src/app/pages/editar-servicios/editar-servicios.component.ts
+++ Angular/src/app/pages/editar-servicios/editar-servicios.component.ts
@@ -13,13 +13,6 @@ import { ServicioService } from 'src/app/service/servicio.service';
 export class EditarServiciosComponent {
   servicio: Servicio = Object();
 
-  nombre: string = "";
-  imagen!: File;
-  descripcion: string = "";
-  precio: string = "";
-  fecha_creacion: string = "";
-
-
   constructor(private servicioService: ServicioService, private activatedRouter: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
@@ -37,62 +30,33 @@ export class EditarServiciosComponent {
   }
 
   guardarNombre(event: any) {
-    console.log(this.nombre = event.target.value)
+    console.log(this.servicio.nombre = event.target.value)
   }
 
   guardarDescripcion(event: any) {
-    console.log(this.descripcion = event.target.value)
+    console.log(this.servicio.descripcion = event.target.value)
   }
 
   guardarPrecio(event: any) {
-    console.log(this.precio = event.target.value)
+    console.log(this.servicio.precio = event.target.value)
   }
 
   guardarFecha(event: any) {
-    console.log(this.fecha_creacion = event.target.value)
+    console.log(this.servicio.fecha_creacion = event.target.value)
   }
 
   enviarFoto(event: any) {
-    console.log(this.imagen = event.target.files[0])
+    console.log(this.servicio.imagen = event.target.files[0])
   }
-
-
-
-  guardar() {
-
-    const servicio = new FormData();
-    servicio.append('nombre', this.nombre);
-    servicio.append('descripcion', this.descripcion);
-    servicio.append('precio', this.precio);
-    servicio.append('fecha_creacion', this.fecha_creacion);
-    servicio.append('imagen', this.imagen, this.imagen!.name);
-
-    let newServicio = new Servicio(this.nombre, this.descripcion, this.precio, this.fecha_creacion, this.imagen);
-
-    console.log("se guardo la modificacion del servicio " + newServicio.nombre);
-    console.log("se guardo la modificacion del servicio " + newServicio.descripcion);
-    console.log("se guardo la modificacion del servicio " + newServicio.precio);
-    console.log("se guardo la modificacion del servicio " + newServicio.fecha_creacion);
-    console.log("se guardo la modificacion del servicio " + newServicio.imagen?.name);
-
-    
-
-  }
-
 
 
   onUpdate(): void {
-   
+
     console.log("se guardo la modificacion del servicio " + this.servicio.nombre);
     console.log("se guardo la modificacion del servicio " + this.servicio.descripcion);
     console.log("se guardo la modificacion del servicio " + this.servicio.precio);
     console.log("se guardo la modificacion del servicio " + this.servicio.fecha_creacion);
 
-    this.servicio.nombre = this.nombre;
-    this.servicio.descripcion = this.descripcion;
-    this.servicio.precio = this.precio;
-    this.servicio.imagen = this.imagen;
-    
     this.servicioService.update(this.servicio.id, this.servicio).subscribe(
       data => {
 
@@ -101,49 +65,24 @@ export class EditarServiciosComponent {
 
       }, err => {
         console.log(err)
-        this.servicio.nombre = this.nombre;
-        this.servicio.descripcion = this.descripcion;
-        this.servicio.precio = this.precio;
-        this.servicio.imagen = this.imagen;
+       
         console.log("Error al modificar la servicio ", this.servicio);
       }
     )
   }
 
 
-  // guardar() {
-  //   const servicioF = new FormData();
-  //   servicioF.append('nombre', this.nombre);
-  //   servicioF.append('descripcion', this.descripcion);
-  //   servicioF.append('precio', this.precio);
-  //   servicioF.append('fecha_creacion', this.fecha_creacion);
-  //   servicioF.append('imagen', this.imagen,this.imagen!.name);
 
-  //   this.servicio.nombre = this.nombre;
-  //   this.servicio.descripcion = this.descripcion;
-  //   this.servicio.precio = this.precio;
-  //   this.servicio.imagen = this.imagen;
 
-  //   console.log("se guardo la modificacion del servicio " + this.servicio.nombre);
 
-  // }
+
+  // submit() {
   // // form = new FormGroup({
   // //   nombre: new FormGroup('', Validators.required),
   // //   imagen: new FormGroup('', Validators.required),
   // //   descripcion: new FormGroup('', Validators.required),
   // //   precio: new FormGroup('', Validators.required),
   // // })
-
-  // // this.data=this.form.value
-  // //     this.servicio.nombre=this.data.nombre
-  // //     this.servicio.imagen=this.data.imagen
-  // //     this.servicio.descripcion=this.data.descripcion
-  // //     this.servicio.precio=this.data.precio
-  // //     console.log(this.data);
-
-  // submit() {
-
-
   //   this.servicioService.update(this.servicio.id, this.servicio).subscribe(
   //     data => {
   //       this.servicio.nombre = this.nombre;
